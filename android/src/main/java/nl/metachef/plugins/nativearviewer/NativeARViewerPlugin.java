7bcde70ff348de32a5e156cca03efbec7b1a1d00
Ivo Ketelaar <ivo@famketelaar.nl>
2025-02-28 11:56:19 +0100

    working demo for android

--- android/src/main/java/nl/metachef/plugins/nativearviewer/NativeARViewerPlugin.java
+++ android/src/main/java/nl/metachef/plugins/nativearviewer/NativeARViewerPlugin.java
@@ -1,22 +1,37 @@
 package nl.metachef.plugins.nativearviewer;
 
-import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
+import com.getcapacitor.annotation.CapacitorPlugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
-import com.getcapacitor.annotation.CapacitorPlugin;
+import android.content.Intent;
+import android.net.Uri;
 
 @CapacitorPlugin(name = "NativeARViewer")
 public class NativeARViewerPlugin extends Plugin {
 
-    private NativeARViewer implementation = new NativeARViewer();
-
-    @PluginMethod
-    public void echo(PluginCall call) {
-        String value = call.getString("value");
-
-        JSObject ret = new JSObject();
-        ret.put("value", implementation.echo(value));
-        call.resolve(ret);
+    @PluginMethod()
+    public void openAR(PluginCall call) {
+        // Extract parameters from JS call if any (e.g., model URL)
+        String intentUrl = call.getString("intent");
+        if (intentUrl == null) {
+            call.reject("No intentUrl provided");
+            return;
+        }
+        // Create the AR Scene Viewer intent
+        Intent sceneViewerIntent = new Intent(Intent.ACTION_VIEW);
+        Uri intentUri
+                = Uri.parse("https://arvr.google.com/scene-viewer/1.0").buildUpon()
+                        .appendQueryParameter("file", "https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/Avocado/glTF/Avocado.gltf")
+                        .appendQueryParameter("mode", "ar_only")
+                        .build();
+        sceneViewerIntent.setData(intentUri);
+        sceneViewerIntent.setPackage("com.google.ar.core");
+        try {
+            getActivity().startActivity(sceneViewerIntent);
+            call.resolve();
+        } catch (Exception e) {
+            call.reject("Failed to launch AR viewer", e);
+        }
     }
 }
--- src/web.ts
+++ src/web.ts
@@ -3,8 +3,8 @@ import { WebPlugin } from '@capacitor/core';
 import type { NativeARViewerPlugin } from './definitions';
 
 export class NativeARViewerWeb extends WebPlugin implements NativeARViewerPlugin {
-  async echo(options: { value: string }): Promise<{ value: string }> {
-    console.log('ECHO', options);
+  async openAR(options: { value: string }): Promise<{ value: string }> {
+    console.log('OPEN AR', options);
     return options;
   }
 }
